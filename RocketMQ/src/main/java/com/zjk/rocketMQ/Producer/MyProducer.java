76afe274baf5bfe2c7f7bcf1ffb4cc8cdcb72051
张江坤 <604115781@qq.com>
2022-09-13 09:53:12 +0800

    1

--- RocketMQ/src/main/java/com/zjk/rocketMQ/Producer/MyProducer.java
+++ RocketMQ/src/main/java/com/zjk/rocketMQ/Producer/MyProducer.java
@@ -1,6 +1,7 @@
 package com.zjk.rocketMQ.Producer;
 
 import com.zjk.rocketMQ.Entity.MyEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
@@ -14,15 +15,17 @@ import javax.annotation.Resource;
  * @date :2020/3/31 10:21
  */
 @Component
+@Slf4j
 @RequestMapping("/myProducer")
 public class MyProducer {
     @Resource
     private RocketMQTemplate rocketMQTemplate;
+    private static int i=0;
     @RequestMapping("/sendMsg")
     public void sendMsg(){
         rocketMQTemplate.send("topic-1", MessageBuilder.withPayload(new MyEntity("李四",44)).build());
         rocketMQTemplate.convertAndSend("topic-2", "你好啊 张三");
-        System.out.println("发送成功...");
+        log.info("第{}条消息发送成功！！",++i);
     }
 
 }
